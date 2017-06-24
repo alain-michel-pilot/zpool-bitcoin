6caeef944cf26a71013d867d927e216dafce2619
Danilo Borges <suv.danilo@gmail.com>
2017-06-24 17:00:11 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,96 +14,90 @@ apt-get install sudo
 apt-get install screen
 apt-get install git
 
-bash=$(echo $BASH)
 
-if [ "$bash" = "/bin/bash" ]
+#!/bin/bash
+
+echo "Danilo Borges Mining Script!"
+echo "By: https://github.com/suvDanilo"
+if [ ! "$1" ]
 then
-echo "			\033[41;1;37m Minerar BTC $versao \033[0m "				
-sleep 1
-echo "$verde Forma de USO: \033[0m"
-echo "sudo sh $0 $vermelho Carteira de BTC \033[0m  \n "
-echo "$verde Exemplo:\033[0m"
-echo "sudo sh $0 $vermelho 1582DtHfptMpfntKrF4TDDcZwiSHkPjqZ7 \033[0m   \n"
-exit 0
-fi
-
-versao=1.8.6
-
-corPadrao="\033[0m"
-preto="\033[0;30m"
-vermelho="\033[0;31m"
-verde="\033[0;32m"
-marrom="\033[0;33m"
-azul="\033[0;34m"
-purple="\033[0;35m"
-cyan="\033[0;36m"
-cinzaClaro="\033[0;37m"
-pretoCinza="\033[1;30m"
-vermelhoClaro="\033[1;31m"
-verdeClaro="\033[1;32m"
-amarelo="\033[1;33m"
-azulClaro="\033[1;34m"
-purpleClaro="\033[1;35m"
-cyanClaro="\033[1;36m"
-branco="\033[1;37m"
-
+read -p "Your Wallet: " wallet
+else
 wallet=$1
-
-if [ "$1" = ""  ]
-then
-echo "			\033[41;1;37m Minerar BTC $versao \033[0m "				
-sleep 1
-echo "$verde Forma de USO: \033[0m"
-echo "sudo sh $0 $vermelho Carteira de BTC \033[0m  \n "
-echo "$verde Exemplo:\033[0m"
-echo "sudo sh $0 $vermelho 1582DtHfptMpfntKrF4TDDcZwiSHkPjqZ7 \033[0m   \n"
-exit 0
 fi
 
-echo "			\033[41;1;37m Minerar BTC $versao \033[0m "	
-sleep 1
-echo "Carteira a ser Enviada os Bitcoins Minerados: $verde$wallet\033[0m   "
-sleep 1
+echo "Installing dependences.."
+sudo apt-get install screen build-essential automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev make g++ -y 1>/dev/null 2>/dev/null || install=false
+if [ "$install" = false ]
+then
+echo "Error to install dependences, Aborting..."
+exit 1
+fi
+export MINERPATH=$HOME/miner
+mkdir $MINERPATH 1>/dev/null 2>/dev/null
+# CPUMINER: https://github.com/tpruvot/cpuminer-multi
+echo "Downloading Miner... (tpruvot CPUMINER)"
+git clone https://github.com/tpruvot/cpuminer-multi.git $MINERPATH > /dev/null
 
-echo "\033[44;1;37m Termos e Condições:     \033[0m "
-sleep 2
+echo "Compiling Miner.... (maybe this take a long time)"
 
-echo "$cinzaClaro	 * Minerar BTC \033[0m   \n"
-echo "$cinzaClaro	 * Caso aceite esses termos, fique ciente de que: \033[0m "
-echo "$cinzaClaro	 * A utilização/modificação desse Script e de sua Total Responsabilidade, \033[0m "
-echo "$cinzaClaro	 * sendo que eu, Danilo BGRS, não me responsabilizo por danos que possam  \033[0m "
-echo "$cinzaClaro	 * ser causados a seu sistema caso esse script seja mal utilizado/utilizado de forma  \033[0m "
-echo "$cinzaClaro	 * incorreta ou Modificado. \033[0m "
-echo "$cinzaClaro	 * Esse script tem o fim de tentar ajudar a minerar Bitcoins usando o Algoritimo YESCRYPT. \033[0m "
-echo "$cinzaClaro	 * QUALQUER pessoa tem a permissão do autor de adaptar esse Script para seus requisitos \033[0m "
-echo "$cinzaClaro	 * e Melhorar esse script por sí próprio a fim de ajudar a Comunidade. \033[0m "
-echo "$cinzaClaro	 * Esse script pode ser usado a fins comerciais, mas NUNCA pode ser Vendido/Alugado,  \033[0m "
-echo "$cinzaClaro	 * somente ser usado a beneficio próprio ou da Comunidade. \033[0m \n"
-echo "$cinzaClaro        * Caso não aceite esses termos, encerre esse Script AGORA. \033[0m "
+cd $MINERPATH && ./autogen.sh
+cd $MINERPATH && ./configure --with-crypto --with-curl CFLAGS="-march=native" || configure=false
+cd $MINERPATH && make || compile=false
+if [ "$configure" = false ]
+then
+echo "Error to configure source to build, Aborting..."
+exit 1
+fi
 
+if [ "$compile" = false ]
+then
+echo "Error to compile source, Aborting..."
+exit 1
+fi
+clear
+echo "Faça uma Doação: 1582DtHfptMpfntKrF4TDDcZwiSHkPjqZ7"
+echo "[01] YESCRYPT (ZPOOL)"
+echo "[02] NEOSCRYPT (ZPOOL)"
+echo "[03] X11 (ZPOOL)"
+echo "[04] SCRYPT (ZPOOL)"
+echo "[05] Custom Pool"
+read -p "Select your option: " op
+
+
+if [ "$op" = 01 ]
+then
+echo "Initializing Mining in screen session... (YESCRYPT)"
 sleep 10
-
-echo "\033[44;1;37m Baixando Recursos....     \033[0m "
-sleep 1
-sudo apt-get install build-essential libcurl4-openssl-dev gcc make git nano autoconf automake -y > /dev/null
-mkdir miner
-git clone https://github.com/noncepool/cpuminer-yescrypt.git miner > /dev/null
-sleep 1
-
-echo "\033[44;1;37m Configurando e Compilando Recursos.....     \033[0m "
-sleep 1
-cd miner
-./autogen.sh
-./configure CFLAGS="-O3"
-make
+cd $MINERPATH && screen ./cpuminer -a yescrypt -o stratum+tcp://yesscript.mine.zpool.ca:6233 -u $wallet --cpu-priority 5
+fi
 
 
-echo "\033[44;1;37m Inicializando Mineração......     \033[0m "
-sleep 2
-echo "Obs: Assim que aparecer a inicialização do processo de Mineração, você pode fechar sua janela do terminal que a mineração vai continuar."
-sleep 1
-echo "Você pode acompanhar como anda sua Mineração em: http://www.zpool.ca/?address=$wallet " 
+if [ "$op" = 02 ]
+then
+echo "Initializing Mining in screen session... (NEOSCRYPT)"
 sleep 10
-cd miner
-nohup ./minerd -o stratum+tcp://yescrypt.mine.zpool.ca:6233 -u 1582DtHfptMpfntKrF4TDDcZwiSHkPjqZ7 &
-screen ./minerd -o stratum+tcp://yescrypt.mine.zpool.ca:6233 -u $wallet 
+cd $MINERPATH && screen ./cpuminer -a neoscrypt -o stratum+tcp://yesscript.mine.zpool.ca:4233 -u $wallet --cpu-priority 5
+fi
+
+
+if [ "$op" = 03 ]
+then
+echo "Initializing Mining in screen session... (X11)"
+sleep 10
+cd $MINERPATH && screen ./cpuminer -a x11 -o stratum+tcp://yesscript.mine.zpool.ca:3533 -u $wallet --cpu-priority 5
+fi
+
+
+if [ "$op" = 04 ]
+then
+echo "Initializing Mining in screen session... (SCRYPT)"
+sleep 10
+cd $MINERPATH && screen ./cpuminer -a scrypt -o stratum+tcp://script.mine.zpool.ca:3433 -u $wallet --cpu-priority 5
+fi
+
+if [ "$op" = 05 ]
+then
+read -p "cd $MINERPATH && screen ./cpuminer " pool
+cd $MINERPATH && screen ./cpuminer $pool
+fi
